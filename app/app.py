62225fa806d3302d7bb9b4dc0076ba74158668e9
Mattaru <gctdljgkjnm1@gmail.com>
2021-01-28 13:04:56 +0200

    initial commit

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 port = int(os.environ.get('PORT', 5000))
 
 
-cache = serialized_redis.JSONSerializedRedis(host='redis', port=6379, db=0)
+cache = serialized_redis.JSONSerializedRedis(host='redis', port=6379)
 
 
 @app.route('/<int:n>')
