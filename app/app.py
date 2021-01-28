4ee7c7e93db6031856ce363131488d64347508cb
Mattaru <gctdljgkjnm1@gmail.com>
2021-01-28 12:58:00 +0200

    initial commit

--- app/app.py
+++ app/app.py
@@ -7,20 +7,6 @@ app = Flask(__name__)
 port = int(os.environ.get('PORT', 5000))
 
 
-def json_serializer(key, value):
-    if type(value) == str:
-        return value, 1
-    return json.dumps(value), 2
-
-
-def json_deserializer(key, value, flags):
-   if flags == 1:
-       return value.decode("utf-8")
-   if flags == 2:
-       return json.loads(value.decode("utf-8"))
-   raise Exception("Unknown serialization format")
-
-
 cache = serialized_redis.JSONSerializedRedis(host='redis', port=6379, db=0)
 
 
