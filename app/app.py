083c54a7ad331c6b42bdf63d5ffced261a0da30f
Mattaru <gctdljgkjnm1@gmail.com>
2021-01-29 10:34:49 +0200

    f. remaked

--- app/app.py
+++ app/app.py
@@ -10,21 +10,40 @@ port = int(os.environ.get('PORT', 5000))
 cache = serialized_redis.JSONSerializedRedis(host='redis', port=6379)
 
 
+# @app.route('/<int:n>')
+# def fibo(n):
+#     cached = None
+#
+#     try:
+#         cached = cache.get(n)
+#     except:
+#         pass
+#
+#     if cached:
+#         return str(cached)
+#     else:
+#         f = fibo(n - 1) + fibo(n - 2)
+#         cache.set(n, f)
+#         return str(f)
 @app.route('/<int:n>')
 def fibo(n):
-    cached = None
+    a = 0
+    b = 1
+    for __ in range(n):
+        cached = None
 
-    try:
-        cached = cache.get(n)
-    except:
-        pass
+        try:
+            cached = cache.get(__)
+        except:
+            pass
 
-    if cached:
-        return str(cached)
-    else:
-        f = fibo(n - 1) + fibo(n - 2)
-        cache.set(n, f)
-        return str(f)
+        if cached:
+            a, b = cache.get(__), a + b
+        else:
+            a, b = b, a + b
+            cache.set(__, a)
+
+    return a
 
 
 if __name__ == '__main__':
